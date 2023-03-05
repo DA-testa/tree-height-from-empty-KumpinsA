9eb7ffd5a7c87ec793943bda52b0afc41590673e
KumpinsA <125810912+KumpinsA@users.noreply.github.com>
2023-03-05 22:58:37 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,9 +2,15 @@ import sys
 import threading
 
 
+<<<<<<< Updated upstream
 def compute_height(n, values):
     nodes = [[] for _ in range(n)]
     for i in range(n):
+=======
+def compute_height(size, values):
+    nodes = [[] for _ in range(size)]
+    for i in range(size):
+>>>>>>> Stashed changes
         if values[i] == -1:
             root = i
         else:
@@ -22,6 +28,7 @@ def compute_height(n, values):
 
 
 def main():
+<<<<<<< Updated upstream
     what_input = input()
 
     if what_input == "I":
@@ -39,3 +46,32 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
+=======
+    what_input = input("I or F: ")
+    
+    if what_input == "F":
+        file_name = input()
+        if "a" in file_name.lower:
+            return
+        else:
+            with open(file_name, "r") as file:
+                size = int(file.readline())
+                values = list(map(int, file.readline().split()))
+    
+    elif (what_input == "I"):
+        size = int(input("Size: "))
+
+        values_str = input("Values: ")
+        values = list(map(int, values_str.split()))
+
+    else:
+        print("Invalid input")
+        main()
+
+    print(compute_height(size, values))
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
+main()
+>>>>>>> Stashed changes
