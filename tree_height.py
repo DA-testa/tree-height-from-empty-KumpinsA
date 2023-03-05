1d42a955d2775ed5b3ce59a2a4211f8e41ff68d3
KumpinsA <125810912+KumpinsA@users.noreply.github.com>
2023-03-05 22:44:04 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,9 +2,9 @@ import sys
 import threading
 
 
-def compute_height(size, values):
-    nodes = [[] for _ in range(size)]
-    for i in range(size):
+def compute_height(n, values):
+    nodes = [[] for _ in range(n)]
+    for i in range(n):
         if values[i] == -1:
             root = i
         else:
@@ -23,29 +23,19 @@ def compute_height(size, values):
 
 def main():
     what_input = input()
-    
-    if what_input == "F":
-        file_name = input()
-        if "a" in file_name.lower:
-            return
-        else:
-            with open(file_name, "r") as file:
-                size = int(file.readline())
-                values = list(map(int, file.readline().split()))
-    
-    elif (what_input == "I"):
-        size = int(input())
 
-        values_str = input()
-        values = list(map(int, values_str.split()))
+    if what_input == "I":
+        n = int(input("Size: "))
+        values = list(map(int, input().split()))
+    elif what_input == "F":
+        return
 
     else:
         print("Invalid input")
-        main()
+        exit()
 
-    print(compute_height(size, values))
+    print(compute_height(n, values))
 
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
